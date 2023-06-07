301305130a8d81766053ca8c3df54ccaec4782c5
Hadrien-Rudich <94840435+Hadrien-Rudich@users.noreply.github.com>
2023-06-07 17:38:20 +0200

    modified prettier/eslint settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -20,5 +20,11 @@ module.exports = {
   plugins: ['@typescript-eslint', 'react', 'prettier'],
   rules: {
     'react/react-in-jsx-scope': 0,
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
 };
