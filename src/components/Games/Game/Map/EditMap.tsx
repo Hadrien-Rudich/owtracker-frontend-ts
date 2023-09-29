2e80e8b4f2c8665385619e657e41e77adb4a9b5e
Hadrien-Rudich <94840435+Hadrien-Rudich@users.noreply.github.com>
2023-09-29 18:33:41 +0200

    map update for game edit WIP

--- src/components/Games/Game/Map/EditMap.tsx
+++ src/components/Games/Game/Map/EditMap.tsx
@@ -1,10 +1,37 @@
+import { useQueries, useQueryClient } from '@tanstack/react-query';
+
 import { useState } from 'react';
 import { gameStore } from '../../../../store/gameStore';
 import type { GameData } from '../../../../types/store/gameTypes';
 import Map from './Map';
+import { gameReportStore } from '../../../../store/gameReportStore';
 
 function EditMap({ gameObj }: { gameObj: GameData }) {
+  // perhaps this whole operation should be done in the EditGame component
+  const queryClient = useQueryClient();
+
+  const queryKey = ['mapsData'];
+
+  const cachedData = queryClient.getQueryData(queryKey);
+
+  if (!cachedData) {
+    useQueries({
+      queries: [
+        {
+          queryKey: ['mapsData'],
+          queryFn: fetchMapsFromApi,
+          onSuccess: (fetchedData: MapData[]) => {
+            addMapsData(fetchedData);
+          },
+          staleTime: 1000 * 60 * 60 * 24 * 30,
+          cacheTime: 1000 * 60 * 60 * 24 * 30 * 30,
+        },
+      ],
+    });
+  }
+
   const { selectedGame, selectedGameMap, selectGameMap } = gameStore();
+  const { mapsData } = gameReportStore();
 
   const [isDropDownActive, setIsDropDownActive] = useState(false);
   const [currentMap, setCurrentMap] = useState<string>(gameObj.map);
@@ -20,7 +47,18 @@ function EditMap({ gameObj }: { gameObj: GameData }) {
     return <Map gameObj={gameObj} />;
   }
 
-  return <div>Edit Map</div>;
+  return (
+    <div className="EditMap_container relative">
+      Edit Map
+      <div className="absolute top-9 right-0 w-full h-40">
+        {mapsData.map((map) => (
+          <p className="bg-fourthColor" key={map.id}>
+            {map.label}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default EditMap;
